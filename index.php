9cf44aefabb077cb952c0349937a8b9ea873087e
aokakes <fulutori@gmail.com>
2020-05-24 23:58:28 +0900

    曜日の表記ズレを修正

--- index.php
+++ index.php
@@ -296,7 +296,6 @@
 	<title>Room Manager</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Refresh" content="600">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
 	<link rel="stylesheet" href="umi/css/bootstrap.min.css">
@@ -392,7 +391,7 @@
 		})
 
 		// 日時取得関数
-		var dayweek = ["月", "火", "水", "木", "金", "土", "日"];
+		var dayweek = ["日", "月", "火", "水", "木", "金", "土"];
 		window.addEventListener("load", function(){
 			var element=document.getElementById("time");
 			setInterval(function(){
